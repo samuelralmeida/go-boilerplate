df2b6ab0a356f2a94ad8180bfba2d0fc625ca158
samuel rodrigues almeida <samuel.ralmeida@gmail.com>
2022-10-21 17:25:23 -0300

    feat: create server pkg to serve application

---
+++ cmd/api/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"flag"
+	"log"
+
+	"github.com/samuelralmeida/go-boilerplate/internal/server"
+)
+
+type config struct {
+	addr string
+	env  string
+	tls  struct {
+		certFile string
+		keyFile  string
+	}
+}
+
+type application struct {
+	config config
+}
+
+func main() {
+	var cfg config
+
+	flag.StringVar(&cfg.addr, "addr", "localhost:4242", "server address to listen on")
+	flag.StringVar(&cfg.env, "env", "development", "environment")
+
+	server := server.New(server.Options{Addr: cfg.addr, Handler: nil, CertFile: cfg.tls.certFile, KeyFile: cfg.tls.keyFile})
+
+	log.Printf("starting server on %s\n", cfg.addr)
+	err := server.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("server stopped")
+}
---
+++ internal/server/server.go
@@ -0,0 +1,94 @@
+package server
+
+import (
+	"context"
+	"crypto/tls"
+	"errors"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+type Options struct {
+	Addr     string
+	Handler  http.Handler
+	CertFile string
+	KeyFile  string
+}
+
+type server struct {
+	srv      *http.Server
+	certFile string
+	keyFile  string
+}
+
+func New(options Options) *server {
+	srv := newServer(options.Addr, options.Handler)
+	return &server{
+		srv:      srv,
+		certFile: options.CertFile,
+		keyFile:  options.KeyFile,
+	}
+}
+
+func newServer(addr string, h http.Handler) *http.Server {
+	return &http.Server{
+		Addr:         addr,
+		Handler:      h,
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 30 * time.Second,
+	}
+}
+
+func (s *server) Run() error {
+	shutdownError := s.makeShutdownErrorChan()
+
+	var err error
+	if s.hasTlsFiles() {
+		err = s.runWithTls()
+	} else {
+		err = s.runWithoutTls()
+	}
+
+	if !errors.Is(err, http.ErrServerClosed) {
+		return err
+	}
+
+	return <-shutdownError
+}
+
+func (s *server) makeShutdownErrorChan() chan error {
+	shutdownError := make(chan error)
+
+	go func() {
+		quit := make(chan os.Signal, 1)
+		signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+		<-quit
+
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+
+		shutdownError <- s.srv.Shutdown(ctx)
+	}()
+
+	return shutdownError
+}
+
+func (s *server) hasTlsFiles() bool {
+	return s.certFile != "" && s.keyFile != ""
+}
+
+func (s *server) runWithTls() error {
+	tlsConfig := &tls.Config{
+		CurvePreferences: []tls.CurveID{tls.X25519, tls.CurveP256},
+	}
+	s.srv.TLSConfig = tlsConfig
+	return s.srv.ListenAndServeTLS(s.certFile, s.keyFile)
+}
+
+func (s *server) runWithoutTls() error {
+	return s.srv.ListenAndServe()
+}
